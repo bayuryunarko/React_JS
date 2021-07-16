4e9d5f72d1f507e54f8c4b5cb56086e678017c4c
Bayu Ramadhan Yunarko <68352049+bayuryunarko@users.noreply.github.com>
2021-07-16 19:27:46 +0700

    Looping Todo Text

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -4,9 +4,30 @@ import React from "react";
 import Todo from "./Todo";
 
 const Todos = () => {
+  const Todos = [
+    {
+      text: "Belajar React 1"
+    },
+    {
+      text: "Belajar React 2"
+    },
+    {
+      text: "Belajar React 3"
+    },
+    {
+      text: "Belajar React 4"
+    },
+    {
+      text: "Belajar React 5"
+    }
+  ];
+
   return (
     <section className="todos">
-      <Todo text="Belajar Props di React" />
+      {/* <Todo text="Belajar Props di React" /> */}
+      {Todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
