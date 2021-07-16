3cad4a217252bd71c9ad2fd362e6821cee07eea5
Bayu Ramadhan Yunarko <68352049+bayuryunarko@users.noreply.github.com>
2021-07-16 18:49:07 +0700

    add Todos component

--- src/App.js
+++ src/App.js
@@ -4,39 +4,14 @@ import "./styles.css";
 import Paper from "./components/Paper";
 import Header from "./components/Header";
 import TodoForm from "./components/TodoForm";
+import Todos from "./components/Todos";
 
 export default function App() {
   return (
     <Paper>
       <Header />
       <TodoForm />
-
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-      </section>
+      <Todos />
     </Paper>
   );
 }
---
+++ src/components/Todos.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+const Todos = () => {
+  return (
+    <section className="todos">
+      <div className="todo">
+        <span className="todo-text">Learning React</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning React</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning React</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning React</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning React</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning React</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning React</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning React</span>
+      </div>
+    </section>
+  );
+};
+
+// Paper.PropTypes = {
+//   children: PropTypes.oneOfType([
+//     PropTypes.arrayOf(PropTypes.node),
+//     PropTypes.node
+//   ])
+// };
+
+export default Todos;
